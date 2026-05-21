59f8d5045120ba5b60660ef650b237eb3d9b08e8
Eralp Uslu <eralpuslubusiness@gmail.com>
2026-05-22 01:17:33 +0300

    feat: Premium yukleme ekrani (initial loader) eklendi ve App.tsx'e entegre edildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react' // Bunu eklemeyi unutma
 import Home from './pages/Home'
 import Services from './pages/Services'
 import Process from './pages/Process'
 import Portfolio from './pages/Portfolio'
 
-export default function App() {
+function App() {
+  // Yükleme ekranını kaldırma mantığı
+  useEffect(() => {
+    const loader = document.getElementById('initial-loader')
+    if (loader) {
+      // Fontların ve kritik bileşenlerin oturması için ufak bir gecikme
+      setTimeout(() => {
+        loader.style.opacity = '0'
+        loader.style.visibility = 'hidden'
+        // Animasyon bittikten sonra DOM'dan tamamen temizle
+        setTimeout(() => {
+          loader.remove()
+        }, 800)
+      }, 500)
+    }
+  }, [])
+
   return (
     <Router>
       <Routes>
@@ -12,8 +29,9 @@ export default function App() {
         <Route path="/hizmetlerimiz" element={<Services />} />
         <Route path="/surecimiz" element={<Process />} />
         <Route path="/calismalarimiz" element={<Portfolio />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   )
 }
+
+export default App
